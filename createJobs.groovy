53cc6576cf96218aabab796a6a41240bbf5d907f
thm <thasnim.hm@gmail.com>
2021-11-01 18:04:29 +0530

    aws changes

--- createJobs.groovy
+++ createJobs.groovy
@@ -20,3 +20,33 @@ pipelineJob('theme-park-job') {
         }
     }
 }
+ipelineJob('theme-park-job-docker') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/thasnim/cloud-demo.git'
+                    }
+                    branch 'main'
+                    scriptPath('Jenkinsfile')
+                }
+            }
+        }
+    }
+}
+ipelineJob('theme-park-job-docker-aws') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/thasnim/cloud-demo.git'
+                    }
+                    branch 'main'
+                    scriptPath('Jenkinsfile-aws')
+                }
+            }
+        }
+    }
+}
